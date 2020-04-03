ae224c7167522173368eb4cb67c1ac7d221719b0
aldettinger <aldettinger@gmail.com>
2020-04-03 16:06:40 +0200

    Initial commit

---
+++ src/test/java/org/apache/camel/quarkus/example/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package org.apache.camel.quarkus.example;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
