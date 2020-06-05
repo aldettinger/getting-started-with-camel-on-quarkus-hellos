ee1a5d6671fc94b082e57a8bf8c0c6c4f58f6d07
aldettinger <aldettinger@gmail.com>
2020-06-05 16:51:28 +0200

    Skipped the pure quarkus side of the demo

--- compare.sh
+++ compare.sh
@@ -1,3 +1,6 @@
+# Set working directory based on script
+cd "$(dirname "$0")"
+
 # Start camel-hello in Quarkus jvm mode
 QUARKUS_JVM_RUNNER='hello-camel-quarkus-jvm-mode/target/hello-camel-quarkus-jvm-mode-1.0-SNAPSHOT-runner'
 java -Dquarkus.http.port=9080 -jar "${QUARKUS_JVM_RUNNER}.jar" 2>&1 > "${QUARKUS_JVM_RUNNER}.log" &
@@ -16,7 +19,7 @@ QUARKUS_NATIVE_PID=$(pgrep -f ${QUARKUS_NATIVE_RUNNER})
 sleep 5s
 
 # Get package size
-QUARKUS_JVM_DISK_SIZE=$(du -sh "${QUARKUS_JVM_RUNNER}.jar" | cut -f1)
+QUARKUS_JVM_DISK_SIZE=$(du -chs "${QUARKUS_JVM_RUNNER}.jar" "hello-camel-quarkus-jvm-mode/target/lib" "${JAVA_HOME}" | tail -n 1 | cut -f1)
 QUARKUS_NATIVE_DISK_SIZE=$(du -sh "${QUARKUS_NATIVE_RUNNER}" | cut -f1)
 
 # Get boot time
@@ -31,7 +34,7 @@ QUARKUS_NATIVE_RSS=$(ps -o rss ${QUARKUS_NATIVE_PID} | sed -n 2p)
 #echo "Quarkus JVM Mode booted in ${QUARKUS_JVM_BOOT_SECONDS} seconds"
 #echo "Quarkus Native Mode booted in ${QUARKUS_NATIVE_BOOT_SECONDS} seconds"
 
-printf "Runtime:Boot Time:Disk Size:Resident Set Size\nQuarkus JVM:${QUARKUS_JVM_BOOT_SECONDS}s:${QUARKUS_JVM_DISK_SIZE}:${QUARKUS_JVM_RSS}K\nQuarkus Native:${QUARKUS_NATIVE_BOOT_SECONDS}s:${QUARKUS_NATIVE_DISK_SIZE}:${QUARKUS_NATIVE_RSS}K\n" | column  -t -s ':'
+printf "Runtime:Camel Boot Time:Disk Size:Resident Set Size\nQuarkus JVM:${QUARKUS_JVM_BOOT_SECONDS}s:${QUARKUS_JVM_DISK_SIZE}:${QUARKUS_JVM_RSS}K\nQuarkus Native:${QUARKUS_NATIVE_BOOT_SECONDS}s:${QUARKUS_NATIVE_DISK_SIZE}:${QUARKUS_NATIVE_RSS}K\n" | column  -t -s ':'
 
 # Killing processes
 kill -9 ${QUARKUS_JVM_PID} ${QUARKUS_NATIVE_PID}
